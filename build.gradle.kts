d1b789743ef6898edfe804522a75cdc0b045aa29
Bruce0203 <BruceFreedy@gmail.com>
2023-02-07 20:20:17 +0900

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 typealias ShadowJar = com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val kotlin_version = "1.7.0"
+val kotlin_version = "1.8.0"
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -11,7 +11,7 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.8.0"
 }
 
 allprojects {
@@ -30,13 +30,21 @@ allprojects {
         maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://libraries.minecraft.net/") }
         maven { url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/") }
-
+        maven { url = uri("https://raw.githubusercontent.com/Bruce0203/library-mirror/spigot-1.19.3/") }
     }
 
     dependencies {
         api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
         compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+        compileOnly("com.mojang:brigadier:1.0.18")
+        compileOnly("commons-io:commons-io:2.6")
+
+        compileOnly("org.projectlombok:lombok:1.18.24")
+        annotationProcessor("org.projectlombok:lombok:1.18.24")
+        testCompileOnly("org.projectlombok:lombok:1.18.24")
+        testAnnotationProcessor("org.projectlombok:lombok:1.18.24")
+
     }
     tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
         dependsOn(tasks.processResources)
